29a6c06f1f3be696676a81bcc2e1876a04fcb51d
Aleksandr Buyanov <abuyan78@yandex.ru>
2018-10-18 04:52:12 -0700

    Thumbnailer is added

---
+++ TestCore517/wwwroot/Scripts/wdvmanager.js
@@ -0,0 +1,52 @@
+﻿var _viewer, _thumb;
+
+// Show status and error messages
+function appendStatus(msg) {
+    var stat = $("#status");
+    stat.append(msg + "<br>");
+}
+
+// Initialize Web Viewing
+$(function () {
+    try {
+        // Initialize Web Document Viewer
+        _viewer = new Atalasoft.Controls.WebDocumentViewer({
+            parent: $('.atala-document-container'),
+            toolbarparent: $('.atala-document-toolbar'),
+            'allowforms': true,
+            allowannotations: true,
+            'savepath': '.',
+            'annotations': {
+                'atala_iuname': 'mm'
+            },
+            serverurl: 'wdv',
+            allowtext: false
+        });
+
+        _thumb = new Atalasoft.Controls.WebDocumentThumbnailer({
+            parent: $('.atala-document-thumbnailer'),
+            serverurl: 'wdv',
+            maxwidth: 120,
+            minwidth: 60,
+            // Note that specify relative URL to our
+            // sample document on server here:
+            viewer: _viewer,
+            allowannotations: true,
+            allowdragdrop: true,
+        });
+
+        // Uncomment the following 2 lines if the toolbar buttons should be displayed without text
+        //$('.atala-ui-button .ui-button-text').html(' ');
+        //$('.atala-ui-button').css({ height: '28px', width: '28px' });
+
+        loadFile();
+
+    } catch (error) {
+        appendStatus("Thrown error: " + error.description);
+    }
+});
+
+
+function loadFile() {
+    _thumb.OpenUrl($('#FileSelectionList').val());
+}
