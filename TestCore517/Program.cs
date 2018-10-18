5074c215ce74a02c59f624aed229542d9434b0af
Aleksandr Buyanov <abuyan78@yandex.ru>
2018-10-18 00:44:49 -0700

    Initial cproject configuration from tutorial

---
+++ TestCore517/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Atalasoft.Imaging.Codec;
+using Atalasoft.Imaging.Codec.Pdf;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TestCore517
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RegisteredDecoders.Decoders.Add(new PdfDecoder());
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
